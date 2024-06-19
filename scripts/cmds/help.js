147bb4f81fb4263c80c8be4086229b6471b7f8dd
Gohime Hatake <146865909+Elohime7@users.noreply.github.com>
2024-06-19 08:31:08 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "【 https://facebook.com/gohime.x.emilya】";
+const doNotDelete = "【 https://facebook.com/the.king.of.ghost.black】";
 /** 
 * @author NTKhang
 * @author: do not delete it
