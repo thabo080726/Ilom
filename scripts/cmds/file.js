245dd0155d5115771199bbf30c63a77736099873
Astaboi768 <160958706+Astaboi768@users.noreply.github.com>
2024-06-29 11:46:08 +0100

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -1,25 +1,18 @@
-const fs = require('fs');
+ const fs = require('fs');
 
 module.exports = {
   config: {
-    name: "sendfile",
-    aliases: ["file"],
+    name: "file",
     version: "1.0",
-    author: "Mahir Tahsan",
+    author: "OtinXShiva",
     countDown: 5,
-    role: 0,
-    shortDescription: "Send bot script",
-    longDescription: "Send bot specified file ",
+    role: 2,
+    description: "Sends code of the file",
     category: "owner",
     guide: "{pn} file name. Ex: .{pn} filename"
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100042484217511",];
-    if (!permission.includes(event.senderID)) {
-      return api.sendMessage("- Paku, You don't have permission to use this command. 🐤", event.threadID, event.messageID);
-    }
-
     const fileName = args[0];
     if (!fileName) {
       return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
@@ -31,6 +24,6 @@ module.exports = {
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');
-    api.sendMessage({ body: fileContent }, event.threadID);
+    api.sendMessage({ body: fileContent }, event.threadID, event.messageID);
   }
 };
