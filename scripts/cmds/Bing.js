2b0f4498b25e08aca111389f700460d65c938d32
Astaboi768 <160958706+Astaboi768@users.noreply.github.com>
2024-07-01 06:55:14 +0100

    Create Bing.js

---
+++ scripts/cmds/Bing.js
@@ -0,0 +1,80 @@
+const axios = require('axios');
+
+module.exports = {
+  config: {
+    name: "bing",
+    aliases: ["b"],
+    version: "1.1",
+    author: "ArYAN/Priyanshi Kaur",
+    countDown: 10,
+    role: 0,
+    shortDescription: {
+      en: 'Text to Image'
+    },
+    longDescription: {
+      en: "Text to image"
+    },
+    category: "image",
+    guide: {
+      en: 'Image Prompt'
+    }
+  },
+
+  onStart: async function ({ api, event, args, message }) {
+    const text = args.join(" ");
+    if (!text) {
+      return message.reply("❓| Please provide a prompt.");
+    }
+
+    let prompt = text;
+    // Remove cookie
+
+    message.reply(`✅| Creating your Imagination...`, async (err, info) => {
+      let ui = info.messageID;
+      api.setMessageReaction("⏳", event.messageID, () => {}, true);
+      try {
+        const response = await axios.get(`https://priyanshi-bing.onrender.com/api/bing?prompt=${prompt}`);
+        api.setMessageReaction("✅", event.messageID, () => {}, true);
+        const images = response.data.url;
+        if (!images) {
+          throw new Error("Images data is missing in the response");
+        }
+        message.unsend(ui);
+        message.reply({
+          body: `🖼 [𝗕𝗜𝗡𝗚] \n━━━━━━━━━━━━\n\nPlease reply with the image number (1, 2, 3, 4) to get the corresponding image in high resolution.`,
+          attachment: await Promise.all(images.map(img => global.utils.getStreamFromURL(img)))
+        }, async (err, info) => {
+          if (err) return console.error(err);
+          global.GoatBot.onReply.set(info.messageID, {
+            commandName: this.config.name,
+            messageID: info.messageID,
+            author: event.senderID,
+            imageUrls: images
+          });
+        });
+      } catch (error) {
+        console.error(error);
+        api.sendMessage(`Error: ${error}`, event.threadID);
+      }
+    });
+  },
+
+  onReply: async function ({ api, event, Reply, args, message }) {
+    const reply = parseInt(args[0]);
+    const { author, imageUrls } = Reply;
+    if (event.senderID !== author) return;
+    try {
+      if (reply >= 1 && reply <= 4) {
+        const img = imageUrls[reply - 1];
+        message.reply({ attachment: await global.utils.getStreamFromURL(img) });
+      } else {
+        message.reply("Invalid image number. Please reply with a number between 1 and 4.");
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+      api.sendMessage(`Error: ${error}`, event.threadID);
+    }
+    message.unsend(Reply.messageID); 
+  },
+}
