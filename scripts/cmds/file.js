0f17b020786e9f8c09e1a2ebee8c28d8933e0789
Astaboi768 <160958706+Astaboi768@users.noreply.github.com>
2024-07-03 01:21:24 +0100

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -1,18 +1,24 @@
- const fs = require('fs');
+const fs = require('fs');
 
 module.exports = {
   config: {
     name: "file",
     version: "1.0",
-    author: "OtinXShiva",
-    countDown: 5,
+    author: "Sarkar",
+    countDown: 1,
     role: 2,
-    description: "Sends code of the file",
+    shortDescription: "File",
+    longDescription: "File",
     category: "owner",
-    guide: "{pn} file name. Ex: .{pn} filename"
+    guide: "{pn} <cmd.js>"
   },
 
   onStart: async function ({ message, args, api, event }) {
+    const subash = ["100090443419982","100091893014482"];
+    if (!subash.includes(event.senderID)) {
+      return api.sendMessage("❌ | You don't have the access!", event.threadID, event.messageID);
+    }
+    
     const fileName = args[0];
     if (!fileName) {
       return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
@@ -20,10 +26,13 @@ module.exports = {
 
     const filePath = __dirname + `/${fileName}.js`;
     if (!fs.existsSync(filePath)) {
-      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+      return api.sendMessage(`File not found: "${fileName}.js"`, event.threadID, event.messageID);
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');
-    api.sendMessage({ body: fileContent }, event.threadID, event.messageID);
+
+    const responseText = `/Cmd install ${fileName}.js ${fileContent}`;
+
+    api.sendMessage({ body: responseText }, event.threadID, event.messageID);
   }
 };
