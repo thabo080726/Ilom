93c7322b2cee844d710397856a89e152e5777e15
Astaboi768 <160958706+Astaboi768@users.noreply.github.com>
2024-07-03 01:10:40 +0100

    Create mage.js

---
+++ scripts/cmds/mage.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+ 
+module.exports = {
+  config: {
+    name: "mage",
+    aliases: ["mage-defusion"], 
+    version: "1.0",
+    author: "Samir Œ",
+    countDown: 5,
+    role: 0,
+    shortDescription: "anime image generator",
+    longDescription: "",
+    category: "𝗔𝗜-𝗚𝗘𝗡𝗘𝗥𝗔𝗧𝗘𝗗",
+    guide: {
+      en: "{pn} <prompt>  --ar 16:9"
+    }
+  },
+ 
+  onStart: async function ({ message, args }) {
+    let prompt = args.join(" ");
+    
+ 
+    try {
+      const apiUrl = `https://samirxpikachu.onrender.com/mageDef?prompt=${encodeURIComponent(prompt)}`;
+      
+      const imageStream = await global.utils.getStreamFromURL(apiUrl);
+ 
+      if (!imageStream) {
+        return message.reply("Failed to retrieve image.");
+      }
+      
+      return message.reply({
+        body: 'here is your image',
+        attachment: imageStream
+      });
+    } catch (error) {
+      console.error(error);
+      return message.reply("Failed to retrieve image.");
+    }
+  }
+}
