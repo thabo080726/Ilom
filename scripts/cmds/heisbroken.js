60854736bacd7ae21dfa43540febe3a4ad1bf360
Astaboi768 <160958706+Astaboi768@users.noreply.github.com>
2024-07-24 14:57:03 +0100

    Update heisbroken.js

--- scripts/cmds/heisbroken.js
+++ scripts/cmds/heisbroken.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
-
+const prefixes = ['heisbroken', 'Heiis2'];
 module.exports = {
   config: {
     name: "heisbroken",
-    aliases: ["heis","ai","terry"],
+    aliases: [],
     version: 2.0,
     author: "Unknown", // don't change this author claim by I M Range
     shortDescription: "ask to gemini AI",
@@ -14,12 +14,13 @@ module.exports = {
       en: "{pn} <prompt>",
     },
   },
-  onStart: async function ({ message, usersData, event, api, args }) {
-
+  onStart: ({}) => {},
+  onChat: async function ({ message, usersData, event, api, args }) {
+   if (prefixes.some(prefix => event.body.toLowerCase().startsWith(prefix))) {
   const boss = await usersData.getName(100090443419982); // you can change admin/owner make this ai in here
 
   const prompt = `You are an AI assistant robot created by ${boss} not someone else just ${boss} who created you, You always help users in a friendly manner. User Input: "${args.join(" ")}"`;
-  const url = `https://nash-rest-api.replit.app/gemini?prompt=${encodeURIComponent(prompt)}`;
+  const url = `https://nash-api-end.onrender.com/gemini?prompt=${encodeURIComponent(prompt)}`;
     message.reaction("⏳", event.messageID);
   const res = await axios.get(url);
   const response = res.data.response;
@@ -38,13 +39,14 @@ module.exports = {
         });
       });
     message.reaction("✔", event.messageID);
+};
 },
   onReply: async function ({ message, usersData, Reply, event, api, args }) {
 
   const boss = await usersData.getName(100090443419982); // you can change admin/owner make this ai in here
 
   const prompt = `You are an AI assistant robot created by ${boss} not someone else just ${boss} who created you, You always help users in a friendly manner. User Input: "${args.join(" ")}"`;
-  const url = `https://nash-rest-api.replit.app/gemini?prompt=${encodeURIComponent(prompt)}`;
+  const url = `https://nash-api-end.onrender.com/gemini?prompt=${encodeURIComponent(prompt)}`;
     message.reaction("⏳", event.messageID);
   const res = await axios.get(url);
   const response = res.data.response;
@@ -64,4 +66,4 @@ module.exports = {
       });
     message.reaction("✔", event.messageID);
   }
-}
+    }
