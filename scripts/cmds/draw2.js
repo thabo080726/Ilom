310d457687b6e9297a48af27bce92d21b8a8026a
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-09-10 22:47:18 +0100

    Create draw2.js

---
+++ scripts/cmds/draw2.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+
+ module.exports = {
+  config: {
+    name: "draw2",
+    version: "1.1",
+    author: "OtinXSandip",
+    countDown: 10,
+    role: 0,
+    shortDescription: {
+      en: 'Text to Image'
+    },
+    longDescription: {
+      en: "Text to image"
+    },
+    category: "image",
+    guide: {
+      en: `{pn} your prompt `
+    }
+  },
+  onStart: async function ({ message, api, args, event }) {
+    const text = args.join(' ');
+
+    if (!text) {
+      return message.reply("😡Please provide a prompt with models");
+    }
+
+
+    const baseURL = `https://ashbina.onrender.com/gen2?prompt=${text}`;
+
+    api.setMessageReaction("⏳", event.messageID, () => {}, true);
+
+    message.reply("✅| Generating please wait.", async (err, info) => {
+      message.reply({
+        attachment: await global.utils.getStreamFromURL(baseURL)
+      });
+      let ui = info.messageID;
+      message.unsend(ui);
+      api.setMessageReaction("✅", event.messageID, () => {}, true);
+    });
+  }
+};￼Enter
